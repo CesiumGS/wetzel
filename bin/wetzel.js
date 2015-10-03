2580dcb38f7ff973b2b5fd469cb3dfd337975441
Patrick Cozzi <pjcozzi@gmail.com>
2015-10-03 15:03:06 -0400

    Work in progress

--- bin/wetzel.js
+++ bin/wetzel.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 "use strict";
+var fs = require('fs');
+var path = require('path');
+var argv = require('minimist')(process.argv.slice(2));
+var defined = require('../lib/defined');
 var generateMarkdown = require('../');
 
-process.stdout.write(generateMarkdown());
+if (!defined(argv._[0]) || defined(argv.h) || defined(argv.help)) {
+    var help = 'Usage: node ' + path.basename(__filename) + ' [path-to-json-schema-file]';
+    process.stdout.write(help);
+    return;
+}
+
+var inputPath = argv._[0];
+var schema = JSON.parse(fs.readFileSync(inputPath));
+
+process.stdout.write(generateMarkdown({
+	schema : schema
+}));
