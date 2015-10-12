db4da645486d2b4fee1690f51eb648a95f19cddc
Patrick Cozzi <pjcozzi@gmail.com>
2015-10-12 10:48:05 -0400

    Added header level command-line argument

--- bin/wetzel.js
+++ bin/wetzel.js
@@ -4,10 +4,12 @@ var fs = require('fs');
 var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
 var defined = require('../lib/defined');
+var defaultValue = require('../lib/defaultValue');
 var generateMarkdown = require('../');
 
 if (!defined(argv._[0]) || defined(argv.h) || defined(argv.help)) {
-    var help = 'Usage: node ' + path.basename(__filename) + ' [path-to-json-schema-file]';
+    var help = 'Usage: node ' + path.basename(__filename) + ' [path-to-json-schema-file] [OPTIONS]\n' +
+               '  -l, --headerLevel      Top-level header. Default: 1';
     process.stdout.write(help);
     return;
 }
@@ -17,5 +19,6 @@ var schema = JSON.parse(fs.readFileSync(filename));
 
 process.stdout.write(generateMarkdown({
     schema : schema,
-    basePath : path.dirname(filename)
+    basePath : path.dirname(filename),
+    headerLevel : defaultValue(defaultValue(argv.l, argv.headerLevel), 1)
 }));
