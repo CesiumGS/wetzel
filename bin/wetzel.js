4b1878eae07570af18e7c126db76bfe2b38d3fa6
Howard Wolosky <HowardWolosky@users.noreply.github.com>
2017-05-04 16:58:02 -0700

    Making autoLinking method a command-line option
    Auto-linking is now disabled by default.
    Users can specify `--al` to do aggressive auto-linking, or they
    can specify `--al=cqo` to auto-link types within code-quotes only.
    This gives us the best of both possible worlds.
    
    Additionally, fixes a bug in Schema4Resolver's `normalizeRequired`
    functionality that caused it to incorrectly normalize certain
    required properties.

--- bin/wetzel.js
+++ bin/wetzel.js
@@ -5,12 +5,14 @@ var path = require('path');
 var argv = require('minimist')(process.argv.slice(2), { boolean : ["w", "suppresswarnings" ]});
 var defined = require('../lib/defined');
 var defaultValue = require('../lib/defaultValue');
-var generateMarkdown = require('../');
+var enums = require('../lib/enums');
+var generateMarkdown = require('../lib/generateMarkdown');
 
 if (!defined(argv._[0]) || defined(argv.h) || defined(argv.help)) {
     var help = 'Usage: node ' + path.basename(__filename) + ' [path-to-json-schema-file] [OPTIONS]\n' +
         '  -l,  --headerLevel        Top-level header. Default: 1\n' +
         '  -p,  --schemaPath         The path string that should be used when generating the schema reference paths.\n' +
+        '  -al, --autoLink           Aggressively auto-inter-link types referenced in descriptions.  Add =cqo to auto-link types that are in code-quotes only.\n' +
         '  -d,  --debug              Provide a path, and this will save out intermediate processing artifacts useful in debugging wetzel.' +
         '  -w,  --suppressWarnings   Will not print out WETZEL_WARNING strings indicating identified conversion problems. Default: false';
     process.stdout.write(help);
@@ -19,6 +21,16 @@ if (!defined(argv._[0]) || defined(argv.h) || defined(argv.help)) {
 
 var filepath = argv._[0];
 var schema = JSON.parse(fs.readFileSync(filepath));
+
+var autoLink = enums.autoLinkOption.off;
+switch (defaultValue(argv.al, argv.autoLink)) {
+    case true:
+        autoLink = enums.autoLinkOption.aggressive;
+        break;
+    case "cqo":
+        autoLink = enums.autoLinkOption.codeQuoteOnly;
+        break;
+}
 
 process.stdout.write(generateMarkdown({
     schema: schema,
@@ -29,4 +41,5 @@ process.stdout.write(generateMarkdown({
     schemaRelativeBasePath: defaultValue(defaultValue(argv.p, argv.schemaPath), null),
     debug: defaultValue(defaultValue(argv.d, argv.debug), null),
     suppressWarnings: defaultValue(defaultValue(argv.w, argv.suppressWarnings), false),
+    autoLink: autoLink,
 }));
