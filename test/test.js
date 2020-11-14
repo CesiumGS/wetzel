d0a72f32419b6dde39804a2b885d85309b3d0137
Ed Mackey <elm19087@gmail.com>
2020-11-14 11:31:43 -0500

    Add golden test results, and compare.

--- test/test.js
+++ test/test.js
@@ -2,11 +2,30 @@
 const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
-//const assert = require('assert');
+const assert = require('assert');
 
 const WETZEL_BIN = 'node ./bin/wetzel.js';
 const SCHEMA_PREFIX = 'test/test-schemas/';
 const OUT_PREFIX = process.env.OUT_PREFIX || 'test/test-output/';
+const GOLDEN_PREFIX = process.env.OUT_PREFIX || 'test/test-golden/';
+
+/**
+ * These tests do not follow typical unit testing guidelines. The final end user never
+ * actually runs wetzel itself, they simply read a document that was produced by wetzel.
+ * As such, we don't bother to probe the innards of wetzel to check if each little API
+ * entry point works. Instead, we have sample schema inputs, and "golden" outputs that
+ * wetzel is expected to produce from those inputs.
+ *
+ * If you hack up the inside of wetzel completely, but the resulting output document
+ * has no diffs at all from what was there before, the tests all pass. If you make an
+ * intentional change to wetzel output, the golden outputs will likewise need to be
+ * updated, revealing the intended change as git diffs. And most importantly, if
+ * some changes to wetzel have unintended concequences for its output formatting,
+ * those will be flagged as test failures. Keep your diffs clean!
+ *
+ * This is basically the strategy that was manually used to test wetzel PRs prior
+ * to any test framework being connected.
+ */
 
 describe('wetzel', function () {
     describe('generated output', function () {
@@ -23,6 +42,7 @@ describe('wetzel', function () {
                 if (index.options.hasOwnProperty(option)) {
                     let outputName = schema.name + '-' + option;
                     let outputPathName = path.join(OUT_PREFIX, outputName);
+                    let goldenPathName = path.join(GOLDEN_PREFIX, outputName);
                     let inputPathName = path.join(SCHEMA_PREFIX, schema.path);
                     let ignore = schema.ignore ? ('-i "' + schema.ignore + '"') : '';
 
@@ -37,6 +57,12 @@ describe('wetzel', function () {
                             }
                             done();
                         });
+                    });
+
+                    it('should match golden ' + outputName, function () {
+                        let outputText = fs.readFileSync(outputPathName).toString();
+                        let goldenText = fs.readFileSync(goldenPathName).toString();
+                        assert.strictEqual(outputText, goldenText);
                     });
                 }
             }
